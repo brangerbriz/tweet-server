8f76375cff17936fee0966dd8c182bacf59e6d97
Brannon Dorsey <brannon@brannondorsey.com>
2018-09-11 13:44:01 -0400

    rewrite to capture all tweets from an account.

--- server.js
+++ server.js
@@ -1,35 +1,22 @@
-require('dotenv').config()
-const fs = require("fs")
+'use strict'
 const express = require('express')
 const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
+const tw = require('./twitter.js')
 
 app.use(express.static(__dirname +'/www'))
 
-const Twit = require('twit')
-const T = new Twit({
-    // goto: https://apps.twitter.com/ for keys
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token: process.env.TWITTER_ACCESS_TOKEN,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-    timeout: 60 * 1000// optional HTTP request timeout to apply to all requests.
-})
-
 io.on('connection', (socket)=>{
     console.log(`socket connected: ${socket.id}`)
-
-    socket.on('get-user-tweets',(opts)=>{
-        // https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-user_timeline.html
-        T.get('statuses/user_timeline',{
-            screen_name: opts.user,
-            count: 200, // max (but can be called again with max_id)
-            // max_id:opts.id, // NOTE was playing with this for recursive calls (for "next page")
-        },(err,data,res)=>{
-            if(err) return console.log(err)
-            else socket.emit('got-tweets',data)
-        })
+    socket.on('get-user-tweets', async (opts) => {
+        try {
+            const tweets = await tw.getUserTweets(opts.user)
+            io.emit('got-user-tweets', null, tweets)
+        } catch (err) {
+            console.error(err)
+            io.emit('got-user-tweets', err, null)
+        }
     })
 })
 
---
+++ twitter.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+const Twit = require('twit')
+const T = new Twit({
+    // goto: https://apps.twitter.com/ for keys
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    timeout: 60 * 1000// optional HTTP request timeout to apply to all requests.
+})
+
+async function getUserTweets(user) {
+    // https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-user_timeline.html
+    
+    const tweets = []
+    let batch = await getUserTweetBatch(user)
+
+    tweets.push(...batch)
+    console.log(`Got ${batch.length} new tweets. Total ${tweets.length}`)
+    while (batch.length > 1) {
+        let id = batch[batch.length - 1].id
+        batch = await getUserTweetBatch(user, id)
+        tweets.push(...batch)
+        console.log(`Got ${batch.length} new tweets. Total ${tweets.length}`)
+    }
+    return tweets.map(tweet => tweet.text)
+}
+
+function getUserTweetBatch(user, maxId) {
+    return new Promise((resolve, reject) => {
+        T.get('statuses/user_timeline', {
+            screen_name: user,
+            count: 200, // max (but can be called again with max_id)
+            max_id: maxId
+        }, (err, data, res) => {
+            if (err) reject(err)
+            else resolve(data)
+        })
+    })
+}
+
+module.exports = {
+    getUserTweets
+}
